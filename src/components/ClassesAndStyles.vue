540b8ee7c289236273dfba9bbc5cf62c4d76666d
innamagkeeva <inna.m79@mail.ru>
2025-05-29 02:34:50 +0300

    работа со стилями

--- src/components/ClassesAndStyles.vue
+++ src/components/ClassesAndStyles.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
 import { reactive, ref, computed } from 'vue'
 
-type BasActiveClothing = true | false
-type BasHasError = boolean
-
 interface ErrorType {
   type: string
 }
@@ -13,10 +10,15 @@ interface BasClassObject {
   activeClothing: boolean
   'text-danger': boolean
 }
+interface BasStyleObject {
+  color: string
+  fontSize: string
+  height: string
+}
 
-const isActiveClothing = ref<BasActiveClothing>(true)
+const isActiveClothing = ref<boolean>(true)
 const error = ref<ErrorType | null>(null)
-const hasError = ref<BasHasError>(false)
+const hasError = ref<boolean>(false)
 const clothClass = ref('cloth')
 const activeClothingClass = ref('activeClothing')
 const errorClass = ref('text-danger')
@@ -31,6 +33,29 @@ const objectClass = computed<BasClassObject>(() => ({
   cloth: true,
   activeClothing: isActiveClothing.value,
   'text-danger': error.value?.type === 'fatal',
+}))
+
+const activeColor = ref<string>('orange')
+const fontSize = ref<number>(30)
+const height = ref<number>(43)
+
+//ВОПРОС: почему в стилях тип вставляется в reactive, а в классах в сонстанту? Это я определила методом тыка, потому что как в классе - выдавало ошибку.
+const styleObject = reactive<BasStyleObject>({
+  color: 'blue',
+  fontSize: '22px',
+  height: '31px',
+})
+
+const colorComputed = ref<string>('saddlebrown')
+const fontSizeComputed = ref<string>('24px')
+const heightComputed = ref<string>('37px')
+const backgroundColorComputed = ref<string>('slategray')
+
+const computedStyle = computed(() => ({
+  color: colorComputed.value,
+  fontSize: fontSizeComputed.value,
+  height: heightComputed.value,
+  backgroundColor: backgroundColorComputed.value,
 }))
 </script>
 
@@ -49,11 +74,30 @@ const objectClass = computed<BasClassObject>(() => ({
   >
     Тернарный оператор в массиве
   </div>
+  <div
+    class="cloth"
+    :style="{ color: activeColor, fontSize: fontSize + 'px', height: height + 'px' }"
+  >
+    Инлайн-стили
+  </div>
+  <div
+    class="cloth"
+    :style="styleObject"
+  >
+    Инлайн-стили.Объектный синтаксис
+  </div>
+  <div
+    class="cloth"
+    :style="computedStyle"
+  >
+    Инлайн-стили. Вычисляемые свойства
+  </div>
 </template>
 <style scoped>
 .cloth {
   width: 500px;
   height: 20px;
+  font-size: 17px;
   border: 3px solid hotpink;
   margin: 0 auto;
   padding: 10px 20px;
