6681bd8bb98c9abafaed1106ca4caf225d667b01
innamagkeeva <inna.m79@mail.ru>
2025-06-05 01:55:56 +0300

    Отрисовка списков продолжение

--- src/components/RenderingLists.vue
+++ src/components/RenderingLists.vue
@@ -1,23 +1,35 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 interface City {
   city: string
+}
+
+interface Hairstyles {
+  title: string
+  publishingHouse: string
+  numberOfCopies: number
 }
 
 const title = ref<string>('Отрисовка списков')
 const cities = ref<City[]>([{ city: 'Москва' }, { city: 'Владикавказ' }])
 const country = ref<string>('Россия')
+
+const myHairstyles: Hairstyles = reactive({
+  title: 'Техники стрижек',
+  publishingHouse: 'Тренд',
+  numberOfCopies: 500,
+})
 </script>
 
 <template>
-  <h1 class="title">{{ title }}</h1>
+  <h1
+    class="title"
+    style="font-size: 40px"
+  >
+    {{ title }}
+  </h1>
   <div class="circuit">
-    <p
-      class="title"
-      style="font-size: 20px"
-    >
-      Отображение элементов массива через v-for
-    </p>
+    <p class="title">Отображение элементов массива через v-for</p>
     <ul>
       <li
         v-for="city in cities"
@@ -26,12 +38,7 @@ const country = ref<string>('Россия')
         {{ city.city }}
       </li>
     </ul>
-    <p
-      class="title"
-      style="font-size: 18px"
-    >
-      второй опциональный параметр у v-for с индексом текущего элемента:
-    </p>
+    <p class="title">второй опциональный параметр у v-for с индексом текущего элемента:</p>
     <ul>
       <li
         v-for="(city, index) in cities"
@@ -40,8 +47,50 @@ const country = ref<string>('Россия')
         {{ index }} : {{ country }} - {{ city.city }}
       </li>
     </ul>
-
-    <p style="color: brown">Продолжение следует))))))))......</p>
+    <p class="title">Отображение свойств объекта через v-for</p>
+    <ul>
+      <li
+        v-for="value in myHairstyles"
+        :key="value"
+      >
+        {{ value }}
+      </li>
+    </ul>
+    <p
+      class="title"
+      style="font-size: 12px"
+    >
+      Второй аргумент для получения имени свойства (ключа объекта):
+    </p>
+    <ul>
+      <li
+        v-for="(value, key) in myHairstyles"
+        :key="key"
+      >
+        {{ key }}: {{ value }}
+      </li>
+    </ul>
+    <p
+      class="title"
+      style="font-size: 12px"
+    >
+      И третий — для индекса:
+    </p>
+    <ul>
+      <li
+        v-for="(value, key, index) in myHairstyles"
+        :key="key"
+      >
+        {{ index }}. {{ key }}: {{ value }}
+      </li>
+    </ul>
+    <p class="title">v-for и диапазоны</p>
+    <span
+      style="color: brown"
+      v-for="n in 10"
+      :key="n"
+      >{{ n }}</span
+    >
   </div>
 </template>
 
@@ -54,13 +103,13 @@ ul {
 .title {
   text-align: center;
   color: darkblue;
-  font-size: 40px;
+  font-size: 20px;
   margin-bottom: 10px;
 }
 
 .circuit {
   width: 500px;
-  height: 400px;
+  height: 900px;
   margin: 0 auto;
   margin-bottom: 10px;
   border: 3px solid greenyellow;
